3838329977f71f9a53c9003cca88cac5ca308dfd
unknown <geitipogi@yahoo.com>
2020-02-26 12:47:39 -0800

    edited exercise 163

--- js/ex_163/index163.js
+++ js/ex_163/index163.js
@@ -1,7 +1,16 @@
 function add() {
-    console.log(arguments);
-    console.log(arguments[0])
+    let args = [];
+    for (var i = 0; i < arguments.length; i++){
+        args.push(arguments[i]);
+    }
+    return args
 }
 
-add(1,2,3,4,5);
-add(6,7,8,9,10,11,12,13,14,15)
+console.log(add(1,2,3,4,5).reduce(function(ini,cur){
+    return ini + cur;
+}));
+console.log(add(6,7,8,9,10,11,12,13,14,15).reduce(function(ini,cur){
+    return ini + cur;
+}));
+
+// console.log(add(1,2,3,4,5))
